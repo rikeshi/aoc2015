2cbc2b6cb6edbbdf9fa8e38245a6a48c4ba0ef79
rikeshi <rikeshi@users.noreply.github.com>
2018-12-04 17:05:43 +0100

    day 6 complete

---
+++ day06/part2.jl
@@ -0,0 +1,64 @@
+using Printf
+
+function create_grid(w, h)
+  zeros(h, w)
+end
+
+function toggle(grid, x, y)
+  grid[x, y] += 2
+end
+
+function on(grid, x, y)
+  grid[x, y] += 1
+end
+
+function off(grid, x, y)
+  if grid[x, y] > 0
+    grid[x, y] -= 1
+  end
+end
+
+function toggle_range(grid, x1, y1, x2, y2)
+  for y = y1:y2
+    for x = x1:x2
+      toggle(grid, x, y)
+    end
+  end
+end
+
+function on_range(grid, x1, y1, x2, y2)
+  for y = y1:y2
+    for x = x1:x2
+      on(grid, x, y)
+    end
+  end
+end
+
+function off_range(grid, x1, y1, x2, y2)
+  for y = y1:y2
+    for x = x1:x2
+      off(grid, x, y)
+    end
+  end
+end
+
+function read_and_apply_ranges(grid, fname)
+  for line in eachline(fname)
+    m = match(r"^(.+)\s(\d+),(\d+) through (\d+),(\d+)$", line)
+    action = m.captures[1]
+    x1, y1, x2, y2 = map(x->parse(Int, x)+1, m.captures[2:end])
+    if action == "toggle"
+      op = toggle_range
+    elseif action == "turn on"
+      op = on_range
+    elseif action == "turn off"
+      op = off_range
+    end
+    op(grid, x1, y1, x2, y2)
+  end
+end
+
+grid = create_grid(1000, 1000)
+read_and_apply_ranges(grid, "input")
+@printf("%-20d\n", sum(grid))
+
